50ecfb9f34fe492bbdc05455b8a3aff85fbd58fb
Hembade <2507178@cognizant.com>
2026-08-04 09:57:49 +0530

    Payment service updated and resolve all the bug..

--- backend/payment-service/src/main/java/com/transport/paymentservice/controller/PaymentAnalyticsController.java
+++ backend/payment-service/src/main/java/com/transport/paymentservice/controller/PaymentAnalyticsController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
+
 @RestController
 @RequestMapping("/api/payments/internal/analytics")
 @RequiredArgsConstructor
